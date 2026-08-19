9fc058590643b5f45369c8cc0b84133b5ba117f2
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-08-19 17:27:23 -0500

    Create FLIPTool-2026-v5.sh script
    Initial creation of the FLIPTool-2026-v5.sh script with a header and comments.

---
+++ FLIPTool-2026-v5.sh
@@ -0,0 +1,15 @@
+#!/bin/env bash
+
+# FLIPTool-2026.sh - Find Largest Installed Packages (FLIP) Tool.
+# Finds largest installed packages and displays their names and sizes.
+
+# TODO: Finish SplitConfig Coding (File split already done.)
+
+###############################################################################################################################################################
+# Main Program #															                                                                                                      		      #
+###############################################################################################################################################################
+
+ DisplayBanner
+ # DisplayFunctions
+
+ Hybrid_Query_1
